bc85f258c64ac67f5ac095f21c258b78a9d4ae20
Saptak Manna <saptakmanna100@gmail.com>
2024-10-15 10:04:45 +0530

    Refactor AuthApi

--- app/src/main/java/com/cravyn/app/features/auth/AuthApi.kt
+++ app/src/main/java/com/cravyn/app/features/auth/AuthApi.kt
@@ -1,7 +1,7 @@
 package com.cravyn.app.features.auth
 
 import com.cravyn.app.data.ApiResponse
-import com.cravyn.app.features.auth.models.DeleteRequestBody
+import com.cravyn.app.features.auth.models.DeleteAccountRequestBody
 import com.cravyn.app.features.auth.models.LoginRequestBody
 import com.cravyn.app.features.auth.models.LoginResponse
 import com.cravyn.app.features.auth.models.RefreshAccessTokenResponse
@@ -9,8 +9,8 @@ import com.cravyn.app.features.auth.models.RegisterRequestBody
 import com.cravyn.app.features.auth.models.RegisterResponse
 import com.cravyn.app.features.auth.models.UpdateAccountRequestBody
 import com.cravyn.app.features.auth.models.UpdateAccountResponse
-import com.cravyn.app.features.auth.models.UpdateProfileRequestBody
-import com.cravyn.app.features.auth.models.UpdateProfileResponse
+import com.cravyn.app.features.auth.models.UpdateProfileImageRequestBody
+import com.cravyn.app.features.auth.models.UpdateProfileImageResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
@@ -18,6 +18,11 @@ import retrofit2.http.PATCH
 import retrofit2.http.POST
 
 interface AuthApi {
+    @POST("/customer/register")
+    suspend fun register(
+        @Body body: RegisterRequestBody
+    ): Response<ApiResponse<RegisterResponse>>
+
     @POST("/customer/login")
     suspend fun login(
         @Body body: LoginRequestBody
@@ -26,25 +31,20 @@ interface AuthApi {
     @POST("/customer/logout")
     suspend fun logout(): Response<ApiResponse<Void>>
 
-    @DELETE("/customer")
-    suspend fun delete(
-        @Body body: DeleteRequestBody
-    ): Response<ApiResponse<Void>>
-
-    @POST("/customer/register")
-    suspend fun register(
-        @Body body: RegisterRequestBody
-    ): Response<ApiResponse<RegisterResponse>>
-
     @PATCH("/customer")
-    suspend fun updateProfile(
-        @Body body: UpdateProfileRequestBody
-    ): Response<ApiResponse<UpdateProfileResponse>>
-
-    @PATCH("/customer/profile-image")
     suspend fun updateAccount(
         @Body body: UpdateAccountRequestBody
     ): Response<ApiResponse<UpdateAccountResponse>>
+
+    @DELETE("/customer")
+    suspend fun deleteAccount(
+        @Body body: DeleteAccountRequestBody
+    ): Response<ApiResponse<Void>>
+
+    @PATCH("/customer/profile-image")
+    suspend fun updateProfileImage(
+        @Body body: UpdateProfileImageRequestBody
+    ): Response<ApiResponse<UpdateProfileImageResponse>>
 
     @PATCH("/customer/refresh-token")
     suspend fun refreshAccessToken(): Response<ApiResponse<RefreshAccessTokenResponse>>
