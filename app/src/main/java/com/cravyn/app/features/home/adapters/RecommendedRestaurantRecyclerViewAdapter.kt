5a1d7da7c0f5aed14e923f8332af923c62a671a7
Sneha Datta <snehadatta431@gmail.com>
2024-11-22 12:48:04 +0530

    Implement home page UI (#2)
    ## Explanation
    This PR creates the UI for the home screen as shown below.
    
    ## Screenshots
    |Light Mode|Dark Mode|
    |--|--|
    
    |![image](https://github.com/user-attachments/assets/cae2ea56-e42d-4817-a077-e4931d329f49)|![image](https://github.com/user-attachments/assets/93960e9c-56da-47b6-b958-f12d4c98af80)|
    
    |![image](https://github.com/user-attachments/assets/74209aa9-82f8-49d6-b7ab-5fd1cc53fae5)|![image](https://github.com/user-attachments/assets/33228897-0660-43c7-9710-2572ea36e5fb)|
    
    ---------
    
    Co-authored-by: Saptak Manna <saptakmanna100@gmail.com>

---
+++ app/src/main/java/com/cravyn/app/features/home/adapters/RecommendedRestaurantRecyclerViewAdapter.kt
@@ -0,0 +1,43 @@
+package com.cravyn.app.features.home.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.cravyn.app.R
+import com.cravyn.app.features.home.models.RestaurantItem
+
+class RecommendedRestaurantRecyclerViewAdapter(
+    private val restaurantItemList: List<RestaurantItem>
+) : RecyclerView.Adapter<RecommendedRestaurantRecyclerViewAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val restaurantImg = itemView.findViewById<ImageView>(R.id.restaurant_image)
+        val offer = itemView.findViewById<TextView>(R.id.discount_percent_text)
+        val maxOffer = itemView.findViewById<TextView>(R.id.discount_max_text)
+        val bestInItem = itemView.findViewById<TextView>(R.id.best_in_item_name)
+        val ratingOfRestaurant = itemView.findViewById<TextView>(R.id.rating_delivery_time_text)
+        val address = itemView.findViewById<TextView>(R.id.restaurant_address_text)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_recommended_restaurant, parent, false)
+        return ViewHolder(view)
+    }
+
+    // Bind data to the ViewHolder
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = restaurantItemList[position]
+
+        holder.restaurantImg.setImageResource(item.restaurantImage)
+        holder.offer.text = item.offerAmount
+        holder.maxOffer.text = item.maxOfferAmount
+        holder.bestInItem.text = item.bestInItemName
+        holder.address.text = item.restaurantAddress
+    }
+
+    override fun getItemCount() = restaurantItemList.size
+}
