ec2b54ffa59c40e47ba7b2cccd1609fc5ba3e028
Saptak Manna <saptakmanna100@gmail.com>
2024-11-26 08:28:25 +0530

    Implement address view model and repo

--- app/src/main/java/com/cravyn/app/features/address/AddressRepository.kt
+++ app/src/main/java/com/cravyn/app/features/address/AddressRepository.kt
@@ -1,11 +1,33 @@
 package com.cravyn.app.features.address
 
 import com.cravyn.app.data.api.ApiResponse
+import com.cravyn.app.features.address.models.RemoveAddressRequestBody
+import com.cravyn.app.features.address.models.SaveAddressRequestBody
+import com.cravyn.app.features.address.models.SaveAddressResponse
+import com.cravyn.app.features.address.models.SavedAddressesResponse
 import com.cravyn.app.features.address.models.SearchedAddressResponseItem
+import com.cravyn.app.features.address.models.SetDefaultAddressRequestBody
+import com.cravyn.app.features.address.models.SetDefaultAddressResponse
 import retrofit2.Response
 
 interface AddressRepository {
     suspend fun searchAddresses(
         address: String
     ): Response<ApiResponse<List<SearchedAddressResponseItem>>>
+
+    suspend fun getSavedAddresses(): Response<ApiResponse<SavedAddressesResponse>>
+
+    suspend fun getDefaultAddress(): Response<ApiResponse<SavedAddressesResponse>>
+
+    suspend fun saveAddress(
+        saveAddressRequestBody: SaveAddressRequestBody
+    ): Response<ApiResponse<SaveAddressResponse>>
+
+    suspend fun removeAddress(
+        removeAddressRequestBody: RemoveAddressRequestBody
+    ): Response<ApiResponse<Unit>>
+
+    suspend fun setDefaultAddress(
+        setDefaultAddressRequestBody: SetDefaultAddressRequestBody
+    ): Response<ApiResponse<SetDefaultAddressResponse>>
 }
--- app/src/main/java/com/cravyn/app/features/address/AddressRepositoryImpl.kt
+++ app/src/main/java/com/cravyn/app/features/address/AddressRepositoryImpl.kt
@@ -1,14 +1,48 @@
 package com.cravyn.app.features.address
 
 import com.cravyn.app.data.api.ApiResponse
+import com.cravyn.app.features.address.models.RemoveAddressRequestBody
+import com.cravyn.app.features.address.models.SaveAddressRequestBody
+import com.cravyn.app.features.address.models.SaveAddressResponse
+import com.cravyn.app.features.address.models.SavedAddressesResponse
 import com.cravyn.app.features.address.models.SearchedAddressResponseItem
+import com.cravyn.app.features.address.models.SetDefaultAddressRequestBody
+import com.cravyn.app.features.address.models.SetDefaultAddressResponse
 import retrofit2.Response
 import javax.inject.Inject
 
 class AddressRepositoryImpl @Inject constructor(
     private val addressApi: AddressApi
 ) : AddressRepository {
-    override suspend fun searchAddresses(address: String): Response<ApiResponse<List<SearchedAddressResponseItem>>> {
+    override suspend fun searchAddresses(
+        address: String
+    ): Response<ApiResponse<List<SearchedAddressResponseItem>>> {
         return addressApi.searchAddresses(address)
+    }
+
+    override suspend fun getSavedAddresses(): Response<ApiResponse<SavedAddressesResponse>> {
+        return addressApi.getSavedAddresses()
+    }
+
+    override suspend fun getDefaultAddress(): Response<ApiResponse<SavedAddressesResponse>> {
+        return addressApi.getDefaultAddress(isDefault = true)
+    }
+
+    override suspend fun saveAddress(
+        saveAddressRequestBody: SaveAddressRequestBody
+    ): Response<ApiResponse<SaveAddressResponse>> {
+        return addressApi.saveAddress(saveAddressRequestBody)
+    }
+
+    override suspend fun removeAddress(
+        removeAddressRequestBody: RemoveAddressRequestBody
+    ): Response<ApiResponse<Unit>> {
+        return addressApi.removeAddress(removeAddressRequestBody)
+    }
+
+    override suspend fun setDefaultAddress(
+        setDefaultAddressRequestBody: SetDefaultAddressRequestBody
+    ): Response<ApiResponse<SetDefaultAddressResponse>> {
+        return addressApi.setDefaultAddress(setDefaultAddressRequestBody)
     }
 }
