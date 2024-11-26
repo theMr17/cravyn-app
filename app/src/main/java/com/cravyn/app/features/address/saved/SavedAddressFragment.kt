d6474fe4f41a0297d8c2ef70c2dae2abbc6463d1
Saptak Manna <saptakmanna100@gmail.com>
2024-11-26 09:14:54 +0530

    Saved address list ui in progress

--- app/src/main/java/com/cravyn/app/features/address/saved/SavedAddressFragment.kt
+++ app/src/main/java/com/cravyn/app/features/address/saved/SavedAddressFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.cravyn.app.data.api.Resource
 import com.cravyn.app.databinding.FragmentSavedAddressBinding
+import com.cravyn.app.features.address.AddressViewModel
 import com.cravyn.app.features.address.search.SearchAddressActivity.Companion.createSearchAddressActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,6 +17,8 @@ import dagger.hilt.android.AndroidEntryPoint
 class SavedAddressFragment : Fragment() {
     private var _binding: FragmentSavedAddressBinding? = null
     private val binding get() = _binding!!
+
+    private val addressViewModel: AddressViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +28,21 @@ class SavedAddressFragment : Fragment() {
 
         binding.addNewAddressButton.setOnClickListener {
             startActivity(createSearchAddressActivity(requireContext()))
+        }
+
+        addressViewModel.getSavedAddresses()
+        addressViewModel.savedAddressesLiveData.observe(viewLifecycleOwner) {
+            when (it) {
+                is Resource.Error -> {
+                    Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
+                }
+
+                is Resource.Loading -> {}
+
+                is Resource.Success -> {
+
+                }
+            }
         }
 
         return binding.root
