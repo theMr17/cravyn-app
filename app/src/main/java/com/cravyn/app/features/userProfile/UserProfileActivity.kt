30cb922595d2f9f3b269307a2d4607c0dde9a178
Sneha Datta <snehadatta431@gmail.com>
2024-11-29 17:42:45 +0530

    Add user profile UI

---
+++ app/src/main/java/com/cravyn/app/features/userProfile/UserProfileActivity.kt
@@ -0,0 +1,21 @@
+package com.cravyn.app.features.userProfile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.cravyn.app.R
+
+class UserProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_user_profile)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
