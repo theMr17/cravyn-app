d777ca9e8ce081f0df80fa19545551aed8dbb9f1
Sneha Datta <snehadatta431@gmail.com>
2024-11-29 20:31:28 +0530

    Modify profile UI

--- app/src/main/java/com/cravyn/app/features/Profile/ProfileFragment.kt
+++ app/src/main/java/com/cravyn/app/features/Profile/ProfileFragment.kt
@@ -1,4 +1,4 @@
-package com.cravyn.app.features.userProfile
+package com.cravyn.app.features.Profile
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [UserProfileFragment.newInstance] factory method to
+ * Use the [ProfileFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class UserProfileFragment : Fragment() {
+class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class UserProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
     companion object {
@@ -50,7 +50,7 @@ class UserProfileFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            UserProfileFragment().apply {
+            ProfileFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
