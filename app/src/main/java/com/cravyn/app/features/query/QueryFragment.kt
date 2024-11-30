ad243323c65f480e8e1d295faee02cdbf33086ec
Saptak Manna <saptakmanna100@gmail.com>
2024-11-30 21:34:39 +0530

    Integrate raise queries api

--- app/src/main/java/com/cravyn/app/features/query/QueryFragment.kt
+++ app/src/main/java/com/cravyn/app/features/query/QueryFragment.kt
@@ -7,9 +7,12 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.cravyn.app.R
 import com.cravyn.app.data.api.Resource
 import com.cravyn.app.databinding.FragmentQueryBinding
 import com.cravyn.app.features.query.adapters.QueriesRecyclerViewAdapter
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputLayout
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -25,6 +28,25 @@ class QueryFragment : Fragment() {
     ): View {
         _binding = FragmentQueryBinding.inflate(inflater, container, false)
 
+        binding.raiseQueryButton.setOnClickListener {
+            val view =
+                LayoutInflater.from(requireContext()).inflate(R.layout.dialog_raise_query, null)
+            MaterialAlertDialogBuilder(requireActivity())
+                .setTitle("Raise Query")
+                .setView(view)
+                .setPositiveButton("Submit") { dialog, _ ->
+                    queryViewModel.raiseQuery(
+                        view.findViewById<TextInputLayout>(R.id.question_text_input_layout).editText?.text.toString()
+                    )
+                    dialog.dismiss()
+                }
+                .setNegativeButton("Cancel") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .create()
+                .show()
+        }
+
         queryViewModel.getQueries()
 
         queryViewModel.queriesLiveData.observe(viewLifecycleOwner) {
@@ -38,6 +60,21 @@ class QueryFragment : Fragment() {
                 is Resource.Success -> {
                     binding.queriesRecyclerView.adapter =
                         QueriesRecyclerViewAdapter(it.data?.customerQueries ?: emptyList())
+                }
+            }
+        }
+
+        queryViewModel.raiseQueryLiveData.observe(viewLifecycleOwner) {
+            when (it) {
+                is Resource.Error -> {
+                    Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
+                }
+
+                is Resource.Loading -> {}
+
+                is Resource.Success -> {
+                    Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
+                    queryViewModel.getQueries()
                 }
             }
         }
