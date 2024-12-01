7b714624745501145e3c284bc94c833778828620
Saptak Manna <saptakmanna100@gmail.com>
2024-12-01 08:15:44 +0530

    Add loading indicator

--- app/src/main/java/com/cravyn/app/features/query/QueryFragment.kt
+++ app/src/main/java/com/cravyn/app/features/query/QueryFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.cravyn.app.data.api.Resource
@@ -50,12 +51,16 @@ class QueryFragment : Fragment() {
         queryViewModel.queriesLiveData.observe(viewLifecycleOwner) {
             when (it) {
                 is Resource.Error -> {
+                    binding.queriesLoadingBar.isVisible = false
                     Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
                 }
 
-                is Resource.Loading -> {}
+                is Resource.Loading -> {
+                    binding.queriesLoadingBar.isVisible = true
+                }
 
                 is Resource.Success -> {
+                    binding.queriesLoadingBar.isVisible = false
                     binding.queriesRecyclerView.adapter =
                         QueriesRecyclerViewAdapter(it.data?.customerQueries ?: emptyList())
                 }
