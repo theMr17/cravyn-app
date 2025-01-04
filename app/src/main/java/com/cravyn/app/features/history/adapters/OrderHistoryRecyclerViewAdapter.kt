34e59a80b867a80e5738eddf46e0995eefaeca29
Sneha Datta <snehadatta431@gmail.com>
2025-01-04 12:21:33 +0530

    Create recycle view

---
+++ app/src/main/java/com/cravyn/app/features/history/adapters/OrderHistoryRecyclerViewAdapter.kt
@@ -0,0 +1,24 @@
+package com.cravyn.app.features.history.adapters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.cravyn.app.databinding.ItemOrderHistoryBinding
+
+class OrderHistoryRecyclerViewAdapter (
+
+): RecyclerView.Adapter<OrderHistoryRecyclerViewAdapter.ViewHolder>() {
+
+    class ViewHolder(val binding: ItemOrderHistoryBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/cravyn/app/features/history/adapters/OrderItemRecyclerViewAdapter.kt
@@ -0,0 +1,23 @@
+package com.cravyn.app.features.history.adapters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.cravyn.app.databinding.ItemOrderHistoryBinding
+
+class OrderItemRecyclerViewAdapter (
+): RecyclerView.Adapter<OrderItemRecyclerViewAdapter.ViewHolder>() {
+
+    class ViewHolder(val binding: ItemOrderHistoryBinding ): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
